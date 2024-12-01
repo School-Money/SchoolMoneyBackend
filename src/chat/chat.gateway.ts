1fc5cb5ee112042c4a080e59565486a9ddc5d90c
olekgolus11 <olekgolus02@gmail.com>
2024-12-01 22:35:21 +0100

    Na co komu dziś wczorajsza miłość

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -27,7 +27,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     private activeUsers = new Map<string, string>(); // Map of socketId -> userId
     private clientRooms = new Map<string, string>(); // Map of socketId -> roomId
 
-    async handleConnection(client: Socket) {
+    handleConnection(client: Socket) {
         console.log(`Client connected: ${client.id}`);
     }
 
@@ -39,7 +39,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @UseGuards(AuthGuard)
     @SubscribeMessage('joinRoomPrivate')
-    async handleJoinRoom(
+    async handleJoinRoomPrivate(
         @ConnectedSocket() client: Socket,
         @MessageBody() payload: { receiverId: string },
         @Req() req,
@@ -55,7 +55,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @UseGuards(AuthGuard)
     @SubscribeMessage('sendMessagePrivate')
-    async handleMessage(
+    async handleSendMessagePrivate(
         @ConnectedSocket() client: Socket,
         @MessageBody() payload: { receiverId: string; content: string },
         @Req() req,
@@ -70,6 +70,76 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
         const { receiverId, content } = payload;
         const chatRoom = await this.chatService.sendPrivateMessage(userId, receiverId, content);
+
+        this.server.to(roomId).emit('receiveMessage', chatRoom.messages);
+    }
+
+    @UseGuards(AuthGuard)
+    @SubscribeMessage('joinRoomClass')
+    async handleJoinRoomClass(
+        @ConnectedSocket() client: Socket,
+        @MessageBody() payload: { classId: string },
+        @Req() req,
+    ) {
+        const userId = req.user.id;
+        const chatRoom = await this.chatService.getClassMessages(userId, payload.classId);
+        const roomId = chatRoom._id.toHexString();
+        client.join(roomId);
+        this.clientRooms.set(client.id, roomId);
+        this.activeUsers.set(client.id, userId);
+        console.log(`Client ${client.id} joined room ${roomId}`);
+    }
+
+    @UseGuards(AuthGuard)
+    @SubscribeMessage('sendMessageClass')
+    async handleSendMessageClass(
+        @ConnectedSocket() client: Socket,
+        @MessageBody() payload: { classId: string; content: string },
+        @Req() req,
+    ) {
+        const userId = req.user.id;
+        const roomId = this.clientRooms.get(client.id);
+
+        if (!roomId) {
+            console.error(`Client ${client.id} is not in any room!`);
+            return;
+        }
+
+        const { classId, content } = payload;
+        const chatRoom = await this.chatService.sendClassMessage(userId, classId, content);
+
+        this.server.to(roomId).emit('receiveMessage', chatRoom.messages);
+    }
+
+    @UseGuards(AuthGuard)
+    @SubscribeMessage('joinRoomHelpdesk')
+    async handleJoinRoomHelpdesk(@ConnectedSocket() client: Socket, @Req() req) {
+        const userId = req.user.id;
+        const chatRoom = await this.chatService.getAdminMessages(userId);
+        const roomId = chatRoom._id.toHexString();
+        client.join(roomId);
+        this.clientRooms.set(client.id, roomId);
+        this.activeUsers.set(client.id, userId);
+        console.log(`Client ${client.id} joined room ${roomId}`);
+    }
+
+    @UseGuards(AuthGuard)
+    @SubscribeMessage('sendMessageHelpdesk')
+    async handleSendMessageHelpdesk(
+        @ConnectedSocket() client: Socket,
+        @MessageBody() payload: { content: string },
+        @Req() req,
+    ) {
+        const userId = req.user.id;
+        const roomId = this.clientRooms.get(client.id);
+
+        if (!roomId) {
+            console.error(`Client ${client.id} is not in any room!`);
+            return;
+        }
+
+        const { content } = payload;
+        const chatRoom = await this.chatService.sendAdminMessage(userId, content);
 
         this.server.to(roomId).emit('receiveMessage', chatRoom.messages);
     }
