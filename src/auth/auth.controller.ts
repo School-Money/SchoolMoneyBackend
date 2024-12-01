0461c1bcd12e49f184d329bc7734e8068e9a489f
Misiek22113 <miskojakub@gmail.com>
2024-12-01 21:39:51 +0100

    add get user details

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ParentLogin, ParentRegister } from 'src/interfaces/parent.interface';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,13 @@ export class AuthController {
     @Post('login')
     async loginUser(@Body() user: ParentLogin) {
         return this.authService.loginUser(user);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('user-info')
+    async getUserInfo(@Request() req) {
+        const { id: userId } = req.user;
+        return this.authService.getUserInfo(userId);
     }
 
     //   If we want to make artificial logout just to check if this user at least exists
