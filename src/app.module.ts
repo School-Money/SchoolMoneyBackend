81d2748bbe62aec66271be83587b1e66f8c100ec
Misiek22113 <miskojakub@gmail.com>
2024-12-17 22:58:24 +0100

    fix multer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { AuthController } from "./auth/auth.controller";
-import { AuthService } from "./auth/auth.service";
-import { AuthModule } from "./auth/auth.module";
-import { ParentModule } from "./parent/parent.module";
-import { ClassModule } from "./class/class.module";
-import { ChildModule } from "./child/child.module";
-import { CollectionModule } from "./collection/collection.module";
-import { PaymentModule } from "./payment/payment.module";
-import { MulterModule } from "@nestjs/platform-express";
-import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { ChatModule } from "./chat/chat.module";
-import { AdminModule } from "./admin/admin.module";
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { ParentModule } from './parent/parent.module';
+import { ClassModule } from './class/class.module';
+import { ChildModule } from './child/child.module';
+import { CollectionModule } from './collection/collection.module';
+import { PaymentModule } from './payment/payment.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { ChatModule } from './chat/chat.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
     imports: [
@@ -25,7 +25,7 @@ import { AdminModule } from "./admin/admin.module";
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
-                uri: configService.get<string>("MONGO_URI"),
+                uri: configService.get<string>('MONGO_URI'),
             }),
         }),
         MulterModule.registerAsync({
@@ -41,9 +41,9 @@ import { AdminModule } from "./admin/admin.module";
 
                         const { entity, id } = req.params;
                         const fileExtension = file.originalname.split('.').pop();
-                        const filename = file.originalname.split(".")[0];
+                        const filename = file.originalname.split('.')[0];
                         return {
-                            folder: "uploads",
+                            folder: 'uploads',
                             format: fileExtension,
                             public_id: `${entity}-${id}-${filename}`,
                         };
