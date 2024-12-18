55695e9e70084099cc2e8d2780f09a29e6b769f8
Gujansko <mateusz.klos150401@gmail.com>
2024-12-19 00:23:54 +0100

    Change port back to 3000

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as multer from 'multer';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(8080);
+    await app.listen(3000);
 
     app.use(multer().any());
     app.use(express.raw({ type: 'multipart/form-data', limit: '10mb' }));
